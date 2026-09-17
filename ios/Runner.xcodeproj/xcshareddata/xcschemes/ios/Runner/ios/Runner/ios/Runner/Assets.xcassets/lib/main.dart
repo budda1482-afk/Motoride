8610a4f07e3fe5eb1b74b1a5f9ec580fa4992ad4
budda1482-afk <budda1482@gmail.com>
2026-09-17 21:19:06 +0100

    Create main.dart

---
+++ ios/Runner.xcodeproj/xcshareddata/xcschemes/ios/Runner/ios/Runner/ios/Runner/Assets.xcassets/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MotoRide',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(body: Center(child: Text('🛞 MotoRide Running!'))),
+    );
+  }
+}
