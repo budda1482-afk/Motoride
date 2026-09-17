10b721f21f28858280a9123835baf8b43819c04c
budda1482-afk <budda1482@gmail.com>
2026-09-17 21:05:57 +0100

    Create main.m

---
+++ ios/Runner.xcodeproj/xcshareddata/xcschemes/ios/Runner/main.m
@@ -0,0 +1,8 @@
+#import <Flutter/Flutter.h>
+#import <UIKit/UIKit.h>
+
+int main(int argc, char* argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  }
+}
